a88d09162b7f72a7cbe26ad262d5568253012318
BalaramPratap <35555297+balarampratap14@users.noreply.github.com>
2020-04-06 23:04:49 +0530

    resolving indentation error

--- application.py
+++ application.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-	from flask import Flask
-	app = Flask(__name__)
-	
+from flask import Flask
+app = Flask(__name__)
 
-	@app.route('/')
-	    def hello_world():
-	        return 'Hello World!'
-	
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
 
-	if __name__ == '__main__':
-	    app.run(host='0.0.0.0')   
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')   
